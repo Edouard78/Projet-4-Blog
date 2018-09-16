b5158c92caec84be497b9f32b4cc39c251f2a035
Edouard JOLTREAU <ed78320@hotmail.fr>
2018-09-16 23:20:02 +0200

    creation class post + attributs et constante de class post

---
+++ model/post.php
@@ -0,0 +1,17 @@
+<?php
+
+class Post
+{
+	protected $_id,
+	          $_author,
+						$_title,
+						$_content,
+						$_creationDate,
+						$_updatingDate,
+
+	const INVALID_AUTHOR = 1;
+	const INVALID_TITLE = 2;
+	const INVALID_CONTENT = 3;
+
+						
+}
