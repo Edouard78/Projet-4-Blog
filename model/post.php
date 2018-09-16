c4b2702ca1a985c91362b91ee45a577ebba4340d
Edouard JOLTREAU <ed78320@hotmail.fr>
2018-09-16 23:33:47 +0200

    creation getters et setters class post

--- model/post.php
+++ model/post.php
@@ -13,5 +13,91 @@ class Post
 	const INVALID_TITLE = 2;
 	const INVALID_CONTENT = 3;
 
-						
+	//GETTERS
+
+	public function id()
+	{
+		return $this->_id;
+	}
+
+	public function author()
+	{
+		return $this->_author;
+	}
+
+	public function title()
+	{
+		return $this->_title;
+	}
+
+	public function content()
+	{
+		return $this->content;
+	}
+
+	public function creationDate()
+	{
+		return $this->_creationDate;
+	}
+
+	public function updatingDate()
+	{
+		return $this->_updatingDate;
+	}
+
+  //SETTERS
+
+	public function setId($id)
+	{
+		$id = (int)$id;
+
+		$this->_id = $id;
+	}
+
+	public function setAuthor($author)
+	{
+		if (!is_string($author) || empty($author))
+		{
+			return self::INVALID_AUTHOR;
+		}
+		else
+		{
+			$this->_author = $author
+		}
+	}
+
+	public function setTitle($title)
+	{
+		if (!is_string($title) || empty($title))
+		{
+			return self::INVALID_AUTHOR;
+		}
+		else
+		{
+			$this->_title = $title;
+		}
+	}
+
+	public function setContent($content)
+	{
+		if (!is_string($content) || empty($content))
+		{
+			return self::INVALID_CONTENT;
+		}
+		else
+		{
+			$this->_content = $content;
+		}
+	}
+
+	public function setCreationDate($creationDate)
+	{
+		$this->_creationDate = $creationDate;
+	}
+
+	public function setUpdatingDate($updatingDate)
+	{
+		$this->_updatingDate = $updatingDate;
+	}
+
 }
