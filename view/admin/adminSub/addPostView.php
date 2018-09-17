246f82f6cf9db01cf5b9e6b1301f2d10fe5fef34
Edouard JOLTREAU <ed78320@hotmail.fr>
2018-09-17 17:14:41 +0200

    creation contenu addPostview.php + creation fichier adminTemplate.php
    contenu addPostview :
    formulaire d'ajout
    + recuperation donnée

---
+++ view/admin/adminSub/addPostView.php
@@ -0,0 +1,32 @@
+<?php
+
+require '../controller/frontend.php';
+
+?>
+
+<?php ob_start(); ?>
+
+<h1>Ajouter un article</h1>
+
+<form action="addPostView.php" method="post">
+
+	Auteur : <input type="text" name="author" /><br />
+  Titre : <input type="text" name="title" /><br />
+  Contenu :<br /><textarea name="content"></textarea><br />
+
+	 <input type="submit" value="Ajouter" />
+</form>
+
+<?php $content = ob_get_clean(); ?>
+
+<?php
+
+if(isset($_POST['author'])){
+	$donnees = array('author' => $_POST['author'] , 'title' => $_POST['title'] , 'content' => $_POST['content']);
+
+  addPost($donnees);
+
+}
+
+require '../admin/adminTemplate.php';
+?>
