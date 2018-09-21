ed0e46dc07a8c3501440ed37f08d0e90df18362e
Edouard JOLTREAU <ed78320@hotmail.fr>
2018-09-21 17:42:44 +0200

    debut creation d'un template admin

--- view/admin/adminTemplate.php
+++ view/admin/adminTemplate.php
@@ -2,9 +2,28 @@
 <html>
    <head>
         <meta charset="utf-8" />
-        <title></title>
+        <title>Page d'amnistration</title>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css" rel="stylesheet">
+        <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet">
+
    </head>
    <body>
-        <?php echo $content ?>
+   	<header>
+   		<h1>Blog De Jean</h1>
+   		<h3>Ecrivain</h3>
+   	</header>
+   	<div class = "row">
+   	    <div class="navbar navbar-default">
+            <ul class="nav navbar-nav">
+                <li class="active"> <a href="#">Articles</a> </li>
+                <li> <a href="#">Commentaires</a> </li>
+            </ul>
+         </div>
+        <div class="contentPage">
+            <?php echo $content ?>
+        </div>
+    </div>
    </body>
 </html>
