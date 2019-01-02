c6e7e0d3cc96192ecb057ad9752db98c1f61538d
Edouard JOLTREAU <ed78320@hotmail.fr>
2019-01-02 20:09:43 +0100

    gestion des erreurs pour les id récupéré dans les URL

--- index.php
+++ index.php
@@ -1,180 +1,296 @@
 <?php
 session_start();
-require('controller/frontend.php');
+require ('controller/frontend.php');
 
 
-if(isset($_GET['action'])){
-	if($_GET['action'] == 'reportComment' && isset($_GET['commentId']) && isset($_GET['postId']))
-	{   
-		verifyReport($_GET['commentId'], $_SESSION['id'], $_GET['postId'] );
-        
-	}
+try {
+if (isset($_GET['action']))
+	{
+
+	/*  USER LOGIN AND SBSCRIBE   */
+
+	/*---------------------------------------
+	USER LOGIN
+	----------------------------------------*/
 
 	if ($_GET['action'] == 'connexion' && isset($_POST['login']))
-	{
+		{
 		$login = $_POST['login'];
 		$password = $_POST['password'];
-
 		authentication($login, $password);
-		
-
-	}
-
-	if($_GET['action'] == 'authenticationPage')
-	{
+		}
+	elseif ($_GET['action'] == 'authenticationPage')
+		{
 		authenticationPage();
-	}
-
-	if($_GET['action'] == 'disconnect'){
+		}
+	elseif ($_GET['action'] == 'adminPage')
+		{
+		listPosts(0, 5);
+		}
+	elseif ($_GET['action'] == 'userPage')
+		{
+		listUserInfos($_SESSION['id']);
+		}
+	elseif ($_GET['action'] == 'disconnect')
+		{
 		session_destroy();
 		header('Location: index.php');
-	}
+		}
 
-	if($_GET['action'] == 'subscribePage')
-	{
+	/*---------------------------------------
+	USER SUBSCRIBE
+	----------------------------------------*/
+
+	elseif ($_GET['action'] == 'subscribe' && isset($_POST['login']))
+		{
+		$data = array(
+			'login' => $_POST['login'],
+			'password' => $_POST['password'],
+			'password2' => $_POST['password2'],
+			'email' => $_POST['email']
+		);
+		subscribe($data);
+		}
+	elseif ($_GET['action'] == 'subscribePage')
+		{
 		subscribePage();
-	}
-	if($_GET['action'] == 'subscribe' && isset($_POST['login']))
-	{   
-		$data = array('login' => $_POST['login'], 'password' => $_POST['password'], 'password2' => $_POST['password2'],  'email' => $_POST['email'] );
-        subscribe($data);
-
-	}
-
-	if($_GET['action'] == 'adminPage'){
-		listPosts(0,5);
-	}
-
-	if ($_GET['action'] == 'userPage')
-	{
-		listUserInfos($_SESSION['id']);
-	}
+		}
 
 
-	if ($_GET['action'] == "home" && !isset($_GET['homePage']))
-	{
-	   $postsListsNb = countPostsLists();
+	/* GUEST/USER SECTION */
 
-	   home($postsListsNb,0, 5);
-	}
+	/*---------------------------------------
+	HOME PAGE
+	----------------------------------------*/
 
-	elseif ($_GET['action'] == "home" && isset($_GET['homePage']) )
-	{
+	// HOME PAGE
+
+	elseif ($_GET['action'] == "home" && !isset($_GET['homePage']))
+		{
 		$postsListsNb = countPostsLists();
-		
+		home($postsListsNb, 0, 5);
+		}
+	// HOME PAGE NUMBER X
+
+	elseif ($_GET['action'] == "home" && isset($_GET['homePage']))
+		{
+		$postsListsNb = countPostsLists();
 		$postsNb = $postsListsNb * 5;
-// Boutton précedent		
-		if($_GET['homePage'] == 0)
-		{
+
+		// NEXT HOME PAGE
+
+		if ($_GET['homePage'] == 0)
+			{
 			header('Location: index.php?action=home');
-		}
-// Boutton suivant
+			}
+
+		// PREVIOUS HOME PAGE
+
 		elseif ($_GET['homePage'] >= $postsNb + 5)
-		{
-		$end = intval($_GET['homePage']) - 5 ;
-		header('Location: index.php?action=home&homePage='.$end);
+			{
+			$end = intval($_GET['homePage']) - 5;
+			header('Location: index.php?action=home&homePage=' . $end);
+			}
+		  else
+			{
+			$start = intval($_GET['homePage']) - 5;
+			$end = intval($_GET['homePage']);
+			$postsListsNb = countPostsLists();
+			home($postsListsNb, $start, $end);
+			}
 		}
 
-		else
-		{
-		$start = intval($_GET['homePage']) - 5;
-		$end = intval($_GET['homePage']);
+	/*---------------------------------------
+	POST UNIQUE PAGE
+	----------------------------------------*/
 
-		
-		$postsListsNb = countPostsLists();
-	   home($postsListsNb, $start, $end);
-		}
-	}
-
-	if($_GET['action'] == "postUnique" && $_GET['id'])
+	elseif ($_GET['action'] == "postUnique")
 	{
+	if (isset($_GET['id']) && $_GET['id'] > 0)
+		{
 		postUnique($_GET['id']);
-	}
-	if($_GET['action'] == 'addComment' && isset($_POST['author']) && $_GET['postId'] && isset($_POST['comment']))
+		}
+	else
 	{
-		$data = array('postId' => $_GET['postId'], 'author' => $_POST['author'], 'comment' => $_POST['comment']);
-		var_dump($data);
-		addComment($data);
-
-
-		header('Location: index.php?action=postUnique&id='.$_GET['postId']);
+		throw new Exception('Aucun identifiant de billet envoyé');
 	}
-
-// ADMINISTRATION
-
-if($_GET['action'] == 'addPostPage')
-{
-	addPostPage();
-	
 }
 
-	if($_GET['action'] == 'addPost' && isset($_POST['author']))
-	{
-		$data = array('author' => $_POST['author'] , 'title' => $_POST['title'] , 'content' => $_POST['content']);
+	// ADD COMMENT
 
+	elseif ($_GET['action'] == 'addComment')
+	{
+	if( isset($_POST['author']) && isset($_POST['comment']) && isset($_GET['postId']) )
+		{
+		$data = array(
+			'postId' => $_GET['postId'],
+			'author' => $_POST['author'],
+			'comment' => $_POST['comment']
+		);
+		addComment($data);
+		header('Location: index.php?action=postUnique&id=' . $_GET['postId']);
+		}
+}
+
+	// REPORT COMMENT
+
+	elseif ($_GET['action'] == 'reportComment' && isset($_GET['commentId']) && isset($_GET['postId']))
+		{
+		verifyReport($_GET['commentId'], $_SESSION['id'], $_GET['postId']);
+		}
+
+
+	/*  ADMIN SECTION  */
+
+	/*---------------------------------------
+	POSTS ADMIN SECTION
+	----------------------------------------*/
+
+	// POSTS  HANDLER PAGE
+
+	elseif ($_GET['action'] == 'postsAdmin')
+		{
+		listPosts(0, 5);
+		}
+
+	// ADD POST
+
+	elseif ($_GET['action'] == 'addPostPage')
+		{
+		addPostPage();
+		}
+	elseif ($_GET['action'] == 'addPost' && isset($_POST['author']))
+		{
+		$data = array(
+			'author' => $_POST['author'],
+			'title' => $_POST['title'],
+			'content' => $_POST['content']
+		);
 		addPost($data);
-		listPosts(0,5);
-		
-	}
-	if($_GET['action'] == 'updatePostDirection' && isset($_GET['id'])){
+		listPosts(0, 5);
+		}
 
+	// UPDATE POST
+
+	elseif ($_GET['action'] == 'updatePostDirection'){
+	if (isset($_GET['id']) && $_GET['id'] > 0)
+		{
 		updatePostPage($_GET['id']);
-	}
-
-	if ($_GET['action'] == 'updatePost' && isset($_POST['author']) && isset($_GET['id']))
+		}
+	else
 	{
-		$data = array('id' => $_GET['id'], 'author' => $_POST['author'], 'title' => $_POST['title'], 'content' => $_POST['content']);
+		throw new Exception('Aucun identifiant de billet envoyé');
+	}
+}
+	
+	elseif ($_GET['action'] == 'updatePost' && isset($_POST['author']) )
+	{
+	if (isset($_GET['id']) && $_GET['id'] > 0)
+	{
+	
+		$data = array(
+			'id' => $_GET['id'],
+			'author' => $_POST['author'],
+			'title' => $_POST['title'],
+			'content' => $_POST['content']
+		);
 		updatePost($data);
 		header('Location: index.php');
-
+		}
+		else {
+		throw new Exception('Aucun identifiant de billet envoyé');
+		}
 	}
-	if($_GET['action'] == 'deletePost' && isset($_GET['id']))
-	{
+	// DELETE POST
+
+	elseif ($_GET['action'] == 'deletePost' && isset($_GET['id']))
+		{
 		deletePost($_GET['id']);
-		listPosts(0,5);
+		listPosts(0, 5);
+		}
 
-	}
+	/*---------------------------------------
+	COMMENTS ADMIN SECTION
+	----------------------------------------*/
 
-	if($_GET['action'] == 'commentsAdmin')
-	{
+	// COMMENTS HANDLER PAGE
+
+	elseif ($_GET['action'] == 'commentsAdmin')
+		{
 		listComments();
-	}
+		}
 
-	if($_GET['action'] == 'deleteComment' && isset($_GET['id']))
-	{
+	// DELETE COMMENTS
+
+	elseif ($_GET['action'] == 'deleteComment') {
+	if (isset($_GET['id']) && $_GET['id'] > 0)
+		{
 		deleteComment($_GET['id']);
 		header('Location: index.php?action=commentsAdmin');
+		}
+	else{
+		throw new Exception('Aucun identifiant de commentaire envoyé');
 	}
-
-	if($_GET['action'] == 'postsAdmin')
-	{
-		listPosts(0,5);
-	}
-
-	if ($_GET['action'] == 'usersAdmin')
-	{
-		listUsers();
-	}
-
-	if ($_GET['action'] == 'updateUser' AND isset($_POST['admin']) AND isset($_GET['id'])){
-
-		$data = array('id' => $_GET['id'], 'admin' => $_POST['admin'] );
-		updateUser($data);
-		listUsers();
-	}
-
-	if ($_GET['action'] == 'deleteUser' AND isset($_GET['id']))
-	{
-		deleteUser($_GET['id']);
-		listUsers();
-
-	}
-
-	
 }
 
-else
-{
+	/*---------------------------------------
+	USER ADMIN SECTION
+	----------------------------------------*/
+
+	// USERS HANDLER PAGE
+
+	elseif ($_GET['action'] == 'usersAdmin')
+		{
+		listUsers();
+		}
+
+	// UPDATE USER
+
+	elseif ($_GET['action'] == 'updateUser' )
+	{
+		if(isset($_POST['admin']))
+		{
+			if( isset($_GET['id']) && $_GET['id'] > 0)
+		  	{
+			$data = array(
+			'id' => $_GET['id'],
+			'admin' => $_POST['admin']
+			);
+			updateUser($data);
+			listUsers();
+			}
+			else{
+			throw new Exception('Aucun identifiant dutilisateur envoyé');
+			}
+		}
+		else{
+			throw new Exception('Aucune valeur de status dutilisateur envoyée');
+		}
+	}
+
+	// DELETE USER
+
+	elseif ($_GET['action'] == 'deleteUser')
+	{
+	if (isset($_GET['id']) && $_GET['id'] > 0)
+		{
+		deleteUser($_GET['id']);
+		listUsers();
+		}
+	else{
+		throw new Exception('Aucun identifiant dutilisateur envoyé');
+	}
+   }
+
+	}
+  else
+	{
 	$postsListsNb = countPostsLists();
 	home($postsListsNb, 0, 5);
+	}
+
+}
+
+catch(Exception $e) {
+    echo 'Erreur : ' . $e->getMessage();
 }
