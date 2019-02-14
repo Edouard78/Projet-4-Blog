a80f1358a151a912fb3cd420dc97ddfdc8bdfdcc
Edouard JOLTREAU <ed78320@hotmail.fr>
2019-02-14 22:04:16 +0100

    correction faille xss dans commentsView.php

--- view/admin/commentsView.php
+++ view/admin/commentsView.php
@@ -24,11 +24,11 @@
 		<tbody>
 
 			<tr>
-				<td><?= $data['creationDateFr'] ?></td>
-				<td><?php echo $data['postId'] ?></td>
-				<td><?= $data['author'] ?></td>
-				<td><?= $data['comment'] ?></td>
-				<td><?= $data['reportedTimes'] ?><span class="glyphicon glyphicon-warning-sign"></span></td>
+				<td><?= htmlspecialchars($data['creationDateFr']) ?></td>
+				<td><?php echo htmlspecialchars($data['postId']) ?></td>
+				<td><?= htmlspecialchars($data['author']) ?></td>
+				<td><?= htmlspecialchars($data['comment']) ?></td>
+				<td><?= htmlspecialchars($data['reportedTimes']) ?><span class="glyphicon glyphicon-warning-sign"></span></td>
 
 				<td>	<a href="index.php?action=deleteComment&amp;id=<?= $data['id'] ?>" class="btn btn-dark" role="button">
 						Supprimer 
